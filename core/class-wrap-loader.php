fe8426a68d7662130a21d65088c899f9c3063794
wisdomrainmusic <alisverissepetiniz4601@gmail.com>
2025-10-25 17:02:24 +0300

    Adjust loader to initialize taxonomy module

--- core/class-wrap-loader.php
+++ core/class-wrap-loader.php
@@ -5,7 +5,12 @@ class WRAP_Loader {
 
     public function init() {
         require_once WRAP_PATH . 'core/class-wrap-cpt.php';
+        require_once WRAP_PATH . 'core/class-wrap-taxonomy.php';
+        
         $cpt = new WRAP_CPT();
         $cpt->register();
+
+        $tax = new WRAP_Taxonomy();
+        $tax->register();
     }
 }
---
+++ core/class-wrap-taxonomy.php
@@ -0,0 +1,37 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class WRAP_Taxonomy {
+
+    public function register() {
+        add_action( 'init', array( $this, 'register_language_taxonomy' ) );
+    }
+
+    public function register_language_taxonomy() {
+
+        $labels = array(
+            'name'              => __( 'Languages', 'wrap' ),
+            'singular_name'     => __( 'Language', 'wrap' ),
+            'search_items'      => __( 'Search Languages', 'wrap' ),
+            'all_items'         => __( 'All Languages', 'wrap' ),
+            'parent_item'       => __( 'Parent Language', 'wrap' ),
+            'parent_item_colon' => __( 'Parent Language:', 'wrap' ),
+            'edit_item'         => __( 'Edit Language', 'wrap' ),
+            'update_item'       => __( 'Update Language', 'wrap' ),
+            'add_new_item'      => __( 'Add New Language', 'wrap' ),
+            'new_item_name'     => __( 'New Language Name', 'wrap' ),
+            'menu_name'         => __( 'Languages', 'wrap' ),
+        );
+
+        $args = array(
+            'hierarchical'      => true,
+            'labels'            => $labels,
+            'show_ui'           => true,
+            'show_admin_column' => true,
+            'query_var'         => true,
+            'rewrite'           => array( 'slug' => 'wrap-language' ),
+        );
+
+        register_taxonomy( 'wrap_language', array( 'wrap_player' ), $args );
+    }
+}
